f043a2b09a02d8b099a232041dc1d7b365ab23b9
Paula Attia <Paulaattia@aucegypt.edu>
2020-09-10 17:38:58 +0200

    Paulaattia

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "voidqueen.h"
+
+
+int main() {
+
+int num, counter=0;
+
+cout<<"Enter the number of Queens\n";
+
+cin>>num;
+
+queen(num);
+
+cout<<"Total Solutions="<< counter;
+
+return 0;
+
+}
